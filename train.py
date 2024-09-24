af71f99c72f0380d43fc400c927d3b4e6575aa37
me <azimjon.ibn.mehmonali@gmail.com>
2024-09-24 19:49:24 +0900

    training refactored more utility functiiions

--- train.py
+++ train.py
@@ -2,17 +2,78 @@ import os
 import torch
 import config
 import model
+import json
 
 from torch.utils.data import DataLoader
+from torch.nn import Module
+from torch.optim import Optimizer
+
 from data import CocoObjectDetection
 from loss import YOLOLoss
-import torchlens as tl
+from utils import prediction_to_list_boxes, nms, draw_boxes
+from tqdm import tqdm
+
+import random
+
+def train_epoch(model: Module, data_loader: DataLoader, criterion: YOLOLoss, optimizer: Optimizer, epoch: int):
+    model.train()
+    device = next(model.parameters()).device
+    loop = tqdm(data_loader, f"Epoch {epoch}")
+    for imgs, targets in loop:
+        imgs = imgs.to(device)
+        targets = targets.to(device)
+
+        preds = model(imgs)
+        loss = criterion(preds, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+
+        loop.set_postfix(loss=loss.item())
+
+        # if random.random() < 0.05:
+        #     break
+
+def validate(model: torch.nn.Module, data_loader: DataLoader):
+    model.eval()
+    device = next(model.parameters()).device
+    results = []
+    loop = tqdm(data_loader, "Validation")
+    for imgs, (ids, (heights, widths)) in loop: # batch inference
+        imgs = imgs.to(device)
+
+        with torch.no_grad():
+            preds = model(imgs).cpu()
+        pred_boxes = nms(prediction_to_list_boxes(preds, config.C), confidence_threshold=0.7)
+
+        for id, height, width, boxes in zip(ids, heights, widths, pred_boxes): # per sample
+            for box in boxes:
+                box[3] *= width.item()
+                box[4] *= height.item()
+                box[5] *= width.item()
+                box[6] *= height.item()
+
+                box[3] = max(box[3] - box[5] / 2, 0)
+                box[4] = max(box[4] -  box[6] / 2, 0)
+                detection = {
+                    "image_id": id.item(),
+                    "category_id": int(box[0]),
+                    "bbox": box[3:],
+                    "score": box[1] * box[2]
+                }
+
+                results.append(detection)
+
+    with open('result.json', 'w') as file:
+        json.dump(results, file, indent=4)
 
 def main():
     device = torch.device('cuda')
 
     train_set = CocoObjectDetection(os.path.join(config.COCO_ROOT, 'images/train2017/'), os.path.join(config.COCO_ROOT, 'annotations/instances_train2017.json'))
-    val_set = CocoObjectDetection(os.path.join(config.COCO_ROOT, 'images/val2017/'), os.path.join(config.COCO_ROOT, 'annotations/instances_val2017.json'))
+    val_set = CocoObjectDetection(os.path.join(config.COCO_ROOT, 'images/val2017/'), os.path.join(config.COCO_ROOT, 'annotations/instances_val2017.json'), is_test=True)
 
     train_loader = DataLoader(
         train_set,
@@ -33,26 +94,12 @@ def main():
     yolo = yolo.to(device)
 
     optimizer = torch.optim.Adam(yolo.parameters(), lr=config.LEARNING_RATE)
-
     criterion = YOLOLoss(S=config.S, B=config.B, C=config.C, lambda_coord=config.LAMBDA_COORD, lambda_noobj=config.LAMBDA_NOOBJ)
-
     checkpoint_folder = 'checkpoints'
 
     for epoch in range(config.EPOCHS):
-        for imgs, targets in train_loader:
-            imgs = imgs.to(device)
-            targets = targets.to(device)
-
-            preds = yolo(imgs)
-            print('std:', preds.std().item())
-            loss = criterion(preds, targets)
-
-            optimizer.zero_grad()
-            loss.backward()
-
-            optimizer.step()
-
-            print(loss.item())
+        train_epoch(yolo, train_loader, criterion, optimizer, epoch)
+        validate(yolo, val_loader)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import torch
 
+from torchvision.transforms.functional import to_pil_image
+
+from PIL import ImageDraw
+
 def intersection_over_union(box1, box2):
     '''
         box1 has shape (..., 4)
@@ -28,24 +32,126 @@ def intersection_over_union(box1, box2):
     box1_area = (box1_1[..., 0] - box1_0[..., 0]) * (box1_1[..., 1] - box1_0[..., 1])
     box2_area = (box2_1[..., 0] - box2_0[..., 0]) * (box2_1[..., 1] - box2_0[..., 1])
 
-    union_area = box1_area + box2_area - intersection_area
+    union_area = box1_area + box2_area - intersection_area + 1e-6
 
     iou = intersection_area / union_area
 
     return iou
 
-def keep_box(pred, C):
+def prediction_to_list_boxes(pred: torch.Tensor, C):
     '''
     pred: Tensor with shape (batch_size, S, S, C + 5 * B)
     C: int, number of classes
 
     Returns:
-    Tensor shaped (batch_size, S, S, 6)
-    last dimension represents (class, confidence, x, y, w, h) - coordinates and sides relative to whole image
+    [ [#image_0 [#box_0 class, class_confidence, box_confidence, x, y, w, h], [#box_1 ...] ], [#image_1] ... ], one element per batch
+    x, y, w, h are respect to image size
     '''
     batch_size, S, _, depth = pred.shape
     B = (depth - C) // 5
 
-    boxes = torch.zeros(batch_size, S, S, 6)
+    # reshaping so that boxes gets own dimension
+    pred_boxes = pred[..., C:] \
+                .reshape(-1, S, S, B, 5)
+    pred_boxes_coord = pred_boxes[..., :4]          # (batch_size, S, S, B, 4)
+    pred_boxes_confidence = pred_boxes[..., 4:5]    # (batch_size, S, S, B)
 
+    class_confidence, best_class = pred[..., :C].max(dim=3, keepdim=True) # (batch_size, S, S, 1), (batch_size, S, S, 1)
+    best_confidence, best_confidence_idx = pred_boxes_confidence.max(dim=3, keepdim=True)
     
+    best_confidence = best_confidence.squeeze(dim=-1)
+    best_coord = torch.gather(
+        pred_boxes_coord,
+        dim=3,
+        index=best_confidence_idx.expand(-1, -1, -1, -1, 4)     # (batch_size, S, S, 4)
+    ).squeeze()
+
+    # convert center coordinates from respect to cell to respect to whole image
+    best_coord[..., 0] += torch.arange(S)
+    best_coord[..., 1] += torch.arange(S).unsqueeze(-1)
+    
+    best_coord[..., :2] /= S
+
+    class_confidence = class_confidence.clamp(0, 1)
+    best_confidence = best_confidence.clamp(0, 1)
+    one_box_per_cell = torch.concat((best_class, class_confidence, best_confidence, best_coord), dim=-1)
+
+    as_list = one_box_per_cell.view(batch_size, S * S, 7).tolist()
+
+    return as_list
+
+def nms(batch_boxes, confidence_threshold=0.8):
+    valid_batch_boxes = []
+    for sample_boxes in batch_boxes:
+        valid_sample_boxes = []
+        for box in sample_boxes:
+            if box[-2] <= 0 or box[-1] <= 0 or \
+               box[-3] < 0 or box[-4] < 0 or \
+               box[-3] > 1 or box[-4] > 1:      # check for positive w, h. and x, y inside the image
+                continue
+            if box[1] * box[2] > confidence_threshold:
+                valid_sample_boxes.append(box)
+        
+        valid_batch_boxes.append(valid_sample_boxes)
+    
+    return valid_batch_boxes
+
+def tensor_to_pil(image: torch.Tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    for c, m, s in zip(image, mean, std):
+        c.mul_(s).add_(m)
+    
+    image = image.clamp(0, 1)
+
+    return to_pil_image(image)
+
+def draw_boxes(image: torch.Tensor, boxes):
+    imsize = image.shape[-1]
+
+    pil_image = tensor_to_pil(image)
+    draw = ImageDraw.Draw(pil_image)
+
+    for box in boxes:
+        x, y, w, h = box[-4:]
+        x *= imsize
+        y *= imsize
+        w *= imsize
+        h *= imsize
+
+        x0 = x - w / 2
+        y0 = y - h / 2
+        x1 = x + w / 2
+        y1 = y + h / 2
+        box = (x0, y0, x1, y1)
+        # print(box)
+        draw.rectangle(box, outline='red', width=2)
+
+    return pil_image
+
+if __name__ == '__main__':
+    import os
+    import config
+    from torch.utils.data import DataLoader
+    from data import CocoObjectDetection
+    
+    train_set = CocoObjectDetection(
+        os.path.join(config.COCO_ROOT, 'images/val2017/'),
+        os.path.join(config.COCO_ROOT, 'annotations/instances_val2017.json'),
+    )
+
+    train_loader = DataLoader(
+        train_set,
+        2,
+        num_workers=config.NUM_WORKERS,
+        drop_last=True,
+        shuffle=False
+    )
+
+    for (imgs, targets) in train_loader:
+        print('imgs', imgs.shape)
+        temp = prediction_to_list_boxes(targets, config.C)
+        for img, boxes in zip(imgs, temp):
+            boxes = filter(lambda x: x[1] > 0.5, boxes)
+            res = draw_boxes(img, boxes)
+
+            res.show()
+            input()
